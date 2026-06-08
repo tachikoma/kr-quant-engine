0ae8c78cbc53fcbbd35b817c56cb533e6d0aa893
Durk-jae Yun <iam00th@hanmail.net>
2026-06-08 11:14:46 +0900

    chore: 429 조건 제거 (KIS 미확인)

--- live_trading/kis/_kis_api_client.py
+++ live_trading/kis/_kis_api_client.py
@@ -100,7 +100,7 @@ class KisApiClient:
                 return self._handle_response(res)
             except KisApiError as e:
                 if attempt < self._max_retries and (
-                    e.http_status == 429 or e.http_status >= 500
+                    e.http_status >= 500
                 ):
                     time.sleep(self._retry_delay)
                     continue
@@ -122,7 +122,7 @@ class KisApiClient:
                 return self._handle_response(res)
             except KisApiError as e:
                 if attempt < self._max_retries and (
-                    e.http_status == 429 or e.http_status >= 500
+                    e.http_status >= 500
                 ):
                     time.sleep(self._retry_delay)
                     continue
